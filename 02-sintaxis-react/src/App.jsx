a496dbf04c8c81b221c07a80dd5437161733c9db
Mateo-Mesa-Gallego06 <mesamateo88@gmail.com>
2025-10-28 13:21:14 -0500

    NavBar Finalizado

---
+++ 02-sintaxis-react/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./App.css"
+import { NavBar } from './components/NavBar'  
+export const App = () => {
+  return (
+    <>
+    <NavBar/>    
+    </>
+  )
+}
---
+++ 02-sintaxis-react/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../stylesheets/NavBar.css"
+
+export const NavBar = () => {
+  return (
+    <div>
+        <div className="barra">
+            <img src="" alt="" />
+            <h2 className='LogoFlor'>🌸</h2>
+            <h2 className='LogoSol'>🌞</h2>
+            <h2 className='Title1'>Extensions</h2>
+        </div>
+    </div>
+  )
+}
