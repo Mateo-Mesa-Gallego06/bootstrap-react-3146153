ea6352c2ae0583f08e5b9c4c9c32b692ded7f56f
Mateo-Mesa-Gallego06 <mesamateo88@gmail.com>
2025-12-02 14:18:02 -0500

    Adelanto

--- 04-pagina-ignnis/src/App.jsx
+++ 04-pagina-ignnis/src/App.jsx
@@ -4,15 +4,21 @@ import { Ignnis } from './components/Ignnis'
 export const App = () => {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success px-4">
-      <h1 className="navbar-brand fw-bold" href="#">IGNNIS </h1>
-      <div className="ms-auto d-flex gap-3">
-        <i className="bi bi-list fs-4"></i>
-        <i className="bi bi-house fs-4"></i>
-        <i className="bi bi-search fs-4"></i>
-        <i className="bi bi-person fs-4"></i>
+    <header className="header-ignnis d-flex justify-content-between align-items-center px-4 py-2">
+      <div className="icon-group d-flex gap-3">
+        <i className="bi bi-list"></i>
+        <img className='logo-ignnis' src="./public/Logo-Ignnis-png.png" alt="" />
+        <i className="bi bi-house"></i>
       </div>
-    </nav>
+      <div className="logo-center">
+        <h1 className='title-ignnis'>IGNNIS</h1>
+      </div>
+      <div className="icon-group d-flex gap-3">
+        <i className="bi bi-person"></i>
+        <i className="bi bi-search"></i>
+        <i className="bi bi-cart"></i>
+      </div>
+    </header>
     <Ignnis/>
     </>
 
--- 04-pagina-ignnis/src/components/ignnis.jsx
+++ 04-pagina-ignnis/src/components/ignnis.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Ignnis = () => {
   return (
-    <div></div>
+    <>
+    <section className="section1 d-flex justify-content-center align-items-center text-center">
+      <div className="quote-box p-4">
+        <p className="quote-text">
+          “Cuando la Tierra deja de hablar, solo los que escuchan pueden sembrar de nuevo.”
+        </p>
+      </div>
+    </section>
+    
+    </>
   )
 }
