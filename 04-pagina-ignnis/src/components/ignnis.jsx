fac6060fefe32d678e17ad32558f430943164f64
Mateo-Mesa-Gallego06 <mesamateo88@gmail.com>
2025-12-04 14:14:37 -0500

    Avance

--- 04-pagina-ignnis/src/components/ignnis.jsx
+++ 04-pagina-ignnis/src/components/ignnis.jsx
@@ -13,7 +13,29 @@ export const Ignnis = () => {
         <i className="bi bi-caret-down"></i>
       </div>
     </section>
-    
+
+    <section className="intro-section d-flex">
+      {/* Columna izquierda */}
+      <div className="intro-left d-flex flex-column justify-content-between p-4">
+        <div className="icon-top">
+          <img src="/public/marca-de-agua.png" alt="imagen de relleno" className="marca-de-agua-image" />
+        </div>
+        <div className="text-center">
+          <h2 className="intro-title">Introducción</h2>
+          <button className="btn btn-dark mt-3">DESCUBRE MÁS</button>
+        </div>
+      </div>
+
+      {/* Columna derecha */}
+      <div className="intro-right position-relative">
+        <img src="/public/imagen-section2.jpg" alt="imagen de relleno" className="intro-image" />
+        <div className="intro-text p-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis eleifend metus sed finibus. Donec id est eget turpis gravida sodales id a massa. Nulla facilisi. Curabitur dui massa, rhoncus eu odio vitae, condimentum lobortis ex.
+          </p>
+        </div>
+      </div>
+    </section>
     </>
   )
 }
